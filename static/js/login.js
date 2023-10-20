efc191459b7dac9d655cbfb5e41f509183179a1b
Mayra Troyano <mayra-troyano@hotmail.com>
2023-10-19 21:37:12 -0500

    update

--- static/js/login.js
+++ static/js/login.js
@@ -1,16 +1,25 @@
-function login() {
-    let email = document.getElementById("email").value
-    let password = document.getElementById("password").value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserLogin = Users.find(user => user.email === email &&  user.password === password)
-   
-
-    if (isUserLogin) {
-        console.log(" j holi");
-        alert('inicio exitoso');
-        window.location.href = "structure.html";
-    }else {
-        console.log(" no holi");
-        alert('Su usuario o contraseña son incorrectos');
+addEventListener("DOMContentLoaded", (e) => {
+    const auth = window.localStorage.getItem("auth", 1);
+    if (auth) {
+        window.location.replace("./structure")
     }
-}
+})
+
+
+const submitButton = document.querySelector(".button--login").childNodes[1];
+/* Esta funcion se debe cambiar cuando funcione base de datos */
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const users = JSON.parse(window.localStorage.getItem("users"));
+    const registerForm = document.forms["loginForm"];
+    const emailInput = registerForm.elements["email"].value;
+    const passwordInput = registerForm.elements["password"].value;
+    if(users) {
+        for (let user of users) {
+            if (user.email === emailInput && user.Password === passwordInput) {
+                window.localStorage.setItem("auth", 1);
+                window.location.replace("./structure")
+            }
+        }
+    }
+})
