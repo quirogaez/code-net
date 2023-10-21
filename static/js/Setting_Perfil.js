9a37962b76d180a03bb1d0d707c86e5e0a8ab884
Jonatan Quintero <joonatam17@gmail.com>
2023-10-20 22:02:55 -0500

    settingprofile

--- static/js/Setting_Perfil.js
+++ static/js/Setting_Perfil.js
@@ -1,20 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const tecnologyInput = document.getElementById('tecnology');
-    const selectedTecnologyImg = document.getElementById('selected-tecnology-img');
-    const tecnologyList = document.getElementById('tecnology-list').querySelectorAll('option');
-    const tecnologySetting = document.querySelector('.tecnology-setting');
 
-    tecnologyInput.addEventListener('input', () => {
-        const searchValue = tecnologyInput.value.toLowerCase();
-        tecnologyList.forEach(option => {
-            if (option.value.toLowerCase() === searchValue) {
-                const imagenSrc = option.getAttribute('data-imagen');
-                selectedTecnologyImg.src = imagenSrc;
-                selectedTecnologyImg.style.display = 'block';
-                tecnologyInput.value = ''; // Limpia el input después de seleccionar una tecnología
-            }
-        });
-    });
+const GOOGLE_MAPS_API_KEY = 'TU_API_KEY';
 
-    // Aquí puedes agregar más lógica para manejar las tecnologías seleccionadas
+const defaultFile = '/static/img/perfilsinfoto.jpg';
+
+const file = document.getElementById('profile-picture');
+const img = document.getElementById ('profile-image');
+
+file.addEventListener( 'change', e => {
+    if ( e.target.files[0] ) {
+        const reader = new FileReader ();
+        reader.onload = function ( e ) {
+            img.src = e.target.result;
+        }
+        reader.readAsDataURL (e.target.files[0])
+    } else {
+        img.src = defaultFile;
+    }
 });
+
+const tecnologySelect = document.getElementById("tecnologys");
+const tecnologyImage = document.getElementById("tecnology-image");
+
+
+tecnologySelect.addEventListener("change", function () {
+    const selectedTecnology = tecnologySelect.value;
+    const imageSources = {
+        figma: "/static/img/10.png",
+        html: "/static/img/9.png",
+        css: "/static/img/8.png"
+        
+    };
+
+    if (imageSources[selectedTecnology]) {
+        tecnologyImage.src = imageSources[selectedTecnology];
+    } else {
+        
+        tecnologyImage.src = "/static/img/code.png";
+    }
+});
+
+
+tecnologySelect.dispatchEvent(new Event("change"));
+
+function loadMap() {
+    const initialCoordinates = { lat: 0, lng: 0 }; 
+    const mapOptions = {
+        center: initialCoordinates,
+        zoom: 2, 
+    };
+    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+document.addEventListener('DOMContentLoaded', function () {
+    loadMap();
+});
+
+
+
