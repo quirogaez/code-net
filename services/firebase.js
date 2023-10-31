c6120af9a5c03fa28a968d8dd0933a05496e40a2
Diego Quiroga <quirogadiego2603@gmail.com>
2023-10-31 02:24:34 -0500

    Integracin API twitch y firebase

---
+++ services/firebase.js
@@ -0,0 +1,57 @@
+// Importa las funciones necesarias del SDK de Firebase y otras dependencias
+import { getAuth } from 'firebase/auth';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import dotenv from 'dotenv';
+// Configuración de Firebase con la información de tu proyecto
+dotenv.config()
+const firebaseConfig = {
+    apiKey: process.env.authDomain,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: "code-net-7a600",
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: "264017678385",
+    appId: "1:264017678385:web:9fd646fe04c9d19cab0510",
+    measurementId: "G-NFVBWE9W87"
+};
+
+// Inicializa la aplicación de Firebase con la configuración
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+// Función para subir imágenes a Firebase Storage
+const uploadImage = async (imagesToPost) => {
+    let i = 0;
+    for (let image in imagesToPost) {
+        
+        console.log(imagesToPost)
+        let url = await uploadProcess(imagesToPost[image], image);
+        i++;
+    }
+}
+
+// Función para procesar la subida de una imagen
+const uploadProcess = (file, filename) => {
+    return new Promise((resolve, reject) => {
+        const storageRef = ref(storage, 'images');
+        const folderRef = ref(storageRef, filename);
+        const uploadTask =  uploadBytesResumable(folderRef, Buffer.from( file.data, 'binary'), {contentType: file.mimetype});
+        
+        // Maneja los eventos de cambio de estado durante la subida
+        uploadTask.on('state_changed', (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+        }, (error) => {
+            reject(error);
+        }, async () => {
+            // Obtiene la URL de descarga de la imagen subida
+            const url = await getDownloadURL(uploadTask.snapshot.ref);
+            console.log('URL', url);
+            resolve(url);
+        });
+    });
+}
+
+// Exporta la función para subir imágenes
+export default uploadImage;
