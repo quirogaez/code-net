4fa3d02db6b61a9b3ed9af01793b11f5130af4e8
Mayra Troyano <mayra-troyano@hotmail.com>
2023-11-04 05:07:35 -0500

    Formulario project, Js que valida formulario, img, tecnologia, urls, boton de verificacion, responsive de la pagina

--- static/js/carousels.js
+++ static/js/carousels.js
@@ -194,19 +194,3 @@ document.addEventListener("DOMContentLoaded",  function() {
   });
   /*-------------------------------final carousel life---------------------------- */
 
-  /*--------------------------------inicio boton de mas------------------------------------------- */
-
-  let masOpcionesVisibles = false;
-  function showPlusOptions() {
-    if(masOpcionesVisibles === false) {
-
-      document.querySelector(".showPlus").style.display = "block";
-      masOpcionesVisibles = true;
-     
-    } else {
-      document.querySelector(".showPlus").style.display = "none";
-      masOpcionesVisibles=false;
-      
-    }
-    /*-------------------------------inicio boton de cerrar seccion----------------- */
-}
