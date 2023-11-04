c22dd2f398c493c42c3de92e7258fad05ee54062
Mayra Troyano <mayra-troyano@hotmail.com>
2023-11-03 22:06:56 -0500

    verify

---
+++ static/js/verifyButton.js
@@ -0,0 +1,6 @@
+const verifyButton = document.querySelector('.sure')
+const closeSure = document.querySelector('.close__modalSure')
+openSure.addEventListener ('click', (e)=> {
+    e.preventDefault();
+    sure.classList.add('sure--show')
+});
