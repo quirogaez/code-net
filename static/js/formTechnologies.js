369142972767231259ab64527b485eea2ce21337
Diego Quiroga <quirogadiego2603@gmail.com>
2023-11-04 01:05:59 -0500

    merge mFeature/formTechnologies

--- static/js/formTechnologies.js
+++
@@ -1,6 +0,0 @@
-const urlContainer = document.querySelector(".url__project");
-const getTechnologiesBotton = document.querySelector(".verify");
-
-
-
-cont 
--- static/js/verifyButton.js
+++ static/js/verifyButton.js
@@ -1,8 +1,19 @@
-const verifyButton = document.getElementById('verify')
+const verifyButton = document.getElementById('verify');
+
+
+
 
 verifyButton.addEventListener ('click', (e)=> {
     e.preventDefault();
-    alert ("verificacion en proceso")
+    const url = document.querySelector(".url__project").value;
+    alert ("verificacion en proceso");
+    let cleanUrl = new URL (url);
+    console.log(cleanUrl.protocol)
+    console.log(cleanUrl.hostname)
+    console.log(cleanUrl.host) 
+    console.log(cleanUrl.origin)
+    
+
 });
 
 
