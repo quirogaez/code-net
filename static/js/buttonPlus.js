c076f46ae49ea4e3e12f27c4c4319390399faddd
Mayra Troyano <mayra-troyano@hotmail.com>
2023-11-06 00:40:45 -0500

    settings

--- static/js/buttonPlus.js
+++ static/js/buttonPlus.js
@@ -5,13 +5,11 @@ function showAddOption() {
         document.querySelector(".showPublication").style.display = "block";
         optionAdd = true;
 
-        // Agregar un event listener para cerrar la ventana modal al hacer clic fuera de ella
         document.addEventListener('click', closeIfClickedOutside);
     } else {
         document.querySelector(".showPublication").style.display = "none";
         optionAdd = false;
 
-        // Eliminar el event listener cuando se cierra la ventana modal
         document.removeEventListener('click', closeIfClickedOutside);
     }
 }
@@ -20,12 +18,10 @@ function closeIfClickedOutside(event) {
     const publication = document.querySelector(".showPublication");
     const buttonPlus = document.querySelector(".buttonPlus");
 
-    // Verificar si el clic se realiza fuera de la ventana modal y del botón para abrir la ventana
     if (event.target !== publication && event.target !== buttonPlus) {
         publication.style.display = 'none';
         optionAdd = false;
 
-        // Eliminar el event listener una vez que se cierra la ventana modal
         document.removeEventListener('click', closeIfClickedOutside);
     }
 }
--- static/js/optionMore.js
+++ static/js/optionMore.js
@@ -5,13 +5,11 @@ function showPlusOptions() {
         document.querySelector(".showPlus").style.display = "block";
         masOpcionesVisibles = true;
 
-        // Agregar un event listener para cerrar la sección "showPlus" al hacer clic fuera de ella
         document.addEventListener('click', closeIfClickedOutsideShowPlus);
     } else {
         document.querySelector(".showPlus").style.display = "none";
         masOpcionesVisibles = false;
 
-        // Eliminar el event listener cuando se cierra la sección "showPlus"
         document.removeEventListener('click', closeIfClickedOutsideShowPlus);
     }
 }
@@ -20,12 +18,10 @@ function closeIfClickedOutsideShowPlus(event) {
     const showPlusSection = document.querySelector(".showPlus");
     const plusButton = document.querySelector(".menu-item3");
 
-    // Verificar si el clic se realiza fuera de la sección "showPlus" y del botón de activación
     if (event.target !== showPlusSection && event.target !== plusButton) {
         showPlusSection.style.display = 'none';
         masOpcionesVisibles = false;
 
-        // Eliminar el event listener una vez que se cierra la sección "showPlus"
         document.removeEventListener('click', closeIfClickedOutsideShowPlus);
     }
 }
