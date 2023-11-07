dffb2949343d5f567fda7576cda377ef25e8754d
Diego Quiroga <quirogadiego2603@gmail.com>
2023-11-07 12:41:51 -0500

    Se agrega configuracion modasl de postform

--- static/js/postForm.js
+++ static/js/postForm.js
@@ -1,8 +1,27 @@
-const fileInput = document.getElementById('fileInput');
+const projectMedia = document.getElementById('fileInput');
 const filePreviewContainer = document.getElementById('filePreviewContainer');
 const filePreview = document.getElementById('filePreview');
 
-fileInput.addEventListener('change', function () {
+// Selecciona el botón de publicación de la publicación
+const buttonPost = document.querySelector(".publish-button");
+// Objeto para almacenar las imágenes seleccionadas
+const fileToPost = {};
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    
+    if (Object.keys(buttonPost).length === 0) {
+        alert('Por favor, complete todos los campos del formulario.');
+        return
+    } else {
+        /* Aqui se activa el modal */
+    }
+
+});
+
+projectMedia.addEventListener('change', function () {
     const file = this.files[0];
     if (file) {
         const reader = new FileReader();
